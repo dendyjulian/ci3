e3fae9f7c6582a418911018968fd13137a87fba2
DESKTOP-Q51VH8S\Dendy Julian Maulana <dendyjulianm08@gmail.com>
2018-04-25 23:14:10 +0700

    Tugas 05 - 06

--- application/views/blogger/create.php
+++ application/views/blogger/create.php
@@ -3,10 +3,36 @@
 	echo validation_errors();
 	echo form_open_multipart('blogger/create')
 ?>
-Judul : <input type="text" class="form-control" name="judul"><br>
-Tgl : <input type="date" class="form-control" name="tgl"><br>
-Author : <input type="text" class="form-control" name="author"><br>
-Konten : <textarea name="konten" class="form-control" style="height:400px;"></textarea><br>
+<?php 
+echo form_open_multipart( 'blogger/create', array('class' => 'needs-validation', 'novalidate' => '')); 
+?>
+
+
+Judul : <input type="text" class="form-control" name="judul" value="<?php echo set_value('judul') ?>"><br>
+Tgl : <input type="date" class="form-control" name="tgl" value="<?php echo set_value('tgl') ?>"><br>
+Author : <input type="text" class="form-control" name="author" value="<?php echo set_value('author') ?>"><br>
+Konten : <textarea name="konten" class="form-control" style="height:400px;" value="<?php echo set_value('konten') ?>"></textarea><br>
 Gambar : <input type="file" name="gambar"><br>
 <input type="submit" class="btn btn-defaule" value="Tambah">
 </div>
+
+<script>
+//Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+</script>
--- application/views/blogger/footer.php
+++ application/views/blogger/footer.php
@@ -18,5 +18,7 @@
           </div>
         </section>
  </section>
+
+ <script type="text/javascript" src="<?php echo base_url('assets/js/custom.js');?>"></script>
 </body>
 </html>
